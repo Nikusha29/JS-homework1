ef9c2ec1d334788bd5cac9fa48cc9c61c02e3106
Nikusha29 <nikakekelidze29@gmail.com>
2024-07-27 16:52:59 +0400

    JS
    JS

---
+++ script.js
@@ -0,0 +1,43 @@
+// დავალება 1:
+
+let x = 50;
+
+if (x < 50) {
+    console.log ('ნაკლებია 50ზე');
+}
+else if (x > 20) {
+    console.log ('მეტია 20ზე');
+} else {
+    console.log ('error');
+}
+
+// დავალება 2:
+
+let UserName = 'მარიამ';
+
+if (UserName == 'მარიამ'){
+    console.log ('true');
+}
+else {
+    console.log ('false');
+}
+
+// დავალება 3:
+
+let Item1 = 'მარიამ'
+
+switch (Item1) {
+    case "მარიამ": {
+        console.log ('true'); }
+       
+        break;
+
+        default: {
+            console.log ('false')}
+
+}
+
+
+
+
+
